ff7fc94c27be04631bca6617b1015c0a22ab3cda
basilbseiso <bb1026@georgetown.edu>
2022-04-01 18:28:26 -0400

    Reducing complexity (groceries), updating game.py file

--- app/groceries.py
+++ app/groceries.py
@@ -58,12 +58,12 @@ for p in products:
     print("..." + p["name"] + "   " + to_usd(p["price"]))
 
 
+import statistics
+
 all_prices = []
 for p in products:
     all_prices.append(float(p["price"]))
-
-import statistics
-avg_price = statistics.median(all_prices)
+    avg_price = statistics.median(all_prices)
 
 print("---------")
 print("AVERAGE PRICE:", to_usd(avg_price))
